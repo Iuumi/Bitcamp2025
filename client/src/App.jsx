1393519e69a7d554f0e1f17c212521f84a2a9cd4
Iremide Akoda <ijakoda2@gmail.com>
2025-04-12 07:36:05 -0400

    Fetching works fr fr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [array, setArray] = useState([])
 
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    setArray(response.data.fruits);
+    console.log(response.data.fruits);
+  }
+
+  useEffect (() => {
+    fetchAPI();
+  },[])
   return (
     <>
       <div>
