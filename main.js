1066b5311f2085ce64ac71e2183a51df88d1c502
Iuumi <167376541+Iuumi@users.noreply.github.com>
2025-04-12 13:32:07 -0400

    When sending a chat, loading animation plays

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
 const typingForm = document.querySelector(".typing-form");
+const chatList = document.querySelector(".chat-list");
 
 let userMessage = null;
 
+//create a new message element and return it
+const createMessageElement = (content, className) => {
+    const div = document.createElement("div");
+    div.classList.add("message", className);
+    div.innerHTML = content;
+    return div;
+}
+
+// Show a loading animation while waiting for API response
+const showLoadingAnimation= () => {
+    const html = `<div class="message-content">
+                    <img src="user.png" alt="Gemini" Image" class="avatar">
+                    <p class="text"></p>
+                    <div class="loading-indicator">
+                        <div class="loading-bar"></div>
+                        <div class="loading-bar"></div>
+                        <div class="loading-bar"></div>
+                    </div>
+                </div>
+                <span class="icon material-symbols-rounded">content_copy</span>`;
+
+    const incomingMessageDiv = createMessageElement(html, "incoming","loading");
+    chatList.appendChild(incomingMessageDiv);
+    }
+    
+//Handle sending outgoing chat messages
 const handleOutgoingChat = () => {
     userMessage = typingForm.querySelector(".typing-input").value.trim();
     if(!userMessage) return; //Exit if no message
 
-    console.log(userMessage);
+    const html = `<div class="message-content">
+                <img src="user.png" alt="User Image" class="avatar">
+                <p class="text"></p>
+            </div>`;
+    const outgoingMessageDiv = createMessageElement(html, "outgoing");
+    outgoingMessageDiv.querySelector(".text").innerText=userMessage;
+    chatList.appendChild(outgoingMessageDiv);
+
+    typingForm.reset() //clear input field
+    setTimeout(showLoadingAnimation, 500); //show loading animation after a delay
 }
+
+
 
 //Prevent default form submission and handle outgoing chat
 typingForm.addEventListener("submit",(e) => {
