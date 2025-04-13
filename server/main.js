8a899db857af5964ec48259bdcfacedafcb1ae56
Iremide Akoda <ijakoda2@gmail.com>
2025-04-12 22:35:00 -0400

    avatars ???

--- server/main.js
+++ server/main.js
@@ -38,7 +38,7 @@ const handleOutgoingChat = async () => {
         const formattedResponse = formatApiResponse(data.response);
 
         const botHtml = `<div class="message-content">
-                            <img src="gemini.png" alt="Gemini Image" class="avatar">
+                            <img src="mealbot.png" alt="Mealbot Image" class="avatar">
                             <div class="text">${formattedResponse}</div>
                          </div>
                          <span class="icon material-symbols-rounded copy-button">content_copy</span>`;
@@ -64,7 +64,7 @@ const handleOutgoingChat = async () => {
         if (loadingDiv) chatList.removeChild(loadingDiv);
 
         const errorHtml = `<div class="message-content">
-                                <img src="gemini.png" alt="Gemini Image" class="avatar">
+                                <img src="mealbot.png" alt="Mealbot Image" class="avatar">
                                 <p class="text">Error: Failed to get response.</p>
                             </div>`;
         const errorDiv = createMessageElement(errorHtml, "incoming", "error");
@@ -101,7 +101,7 @@ const createMessageElement = (content, className, ...additionalClasses) => {
 
 const showLoadingAnimation = () => {
     const html = `<div class="message-content">
-                    <img src="user.png" alt="Gemini Image" class="avatar">
+                    <img src="user.png" alt="Mealbot Image" class="avatar">
                     <p class="text"></p>
                     <div class="loading-indicator">
                         <div class="loading-bar"></div>
